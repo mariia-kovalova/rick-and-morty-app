3655e49692bb8d4f27a63bc9af5d6226e1c97814
SvitlanaHonchar <snidlty@gmail.com>
2023-05-14 17:32:46 +0300

    footer styles+html

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -1,13 +1,31 @@
 import { Logo } from 'shared/components/Logo';
 import { Container } from 'shared/styles/components/Container.styled';
+import { StyledDiv, StyledFooter } from './Footer.styled';
+import sprite from '../../shared/icons/sprite.svg';
 
 export const Footer = () => {
   return (
-    <footer>
+    <StyledFooter>
       <Container>
-        Footer
-        <Logo />
+        <StyledDiv>
+          <div className="container-logo">
+            <Logo />
+          </div>
+
+          <div className="container-madeBy">
+            <p>-- Made by PixelSquad --</p>
+            <a href="https://github.com/mariia-kovalova/rick-and-morty-app">
+              [ Github ]
+            </a>
+          </div>
+          <div className="container-support">
+            <svg width="50" height="50">
+              <use href={`${sprite}#icons8-summer-smith`} />
+            </svg>
+            <a href="https://itis.netlify.app/info">Support PixelSquad</a>
+          </div>
+        </StyledDiv>
       </Container>
-    </footer>
+    </StyledFooter>
   );
 };
--- src/modules/Footer/Footer.styled.jsx
+++ src/modules/Footer/Footer.styled.jsx
@@ -1,1 +1,39 @@
 import styled from '@emotion/styled';
+
+export const StyledFooter = styled.footer``;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  border-top: 1px solid ${({ theme }) => theme.textPrimary};
+  font-size: 20px;
+  font-weight: 500;
+
+  & .container-logo {
+    flex: 1;
+  }
+
+  & .container-madeBy {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & .container-support {
+    flex: 1;
+
+    display: flex;
+    /* flex-direction: column; */
+    align-items: center;
+    justify-content: flex-end;
+  }
+  & use {
+    height: 50px;
+  }
+`;
