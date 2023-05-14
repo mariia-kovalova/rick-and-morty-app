1eb44ffd89ec56e15a4bed6fd34aea4d8233a85b
SvitlanaHonchar <snidlty@gmail.com>
2023-05-14 17:33:18 +0300

    header styles+html

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -2,14 +2,21 @@ import { Logo } from 'shared/components/Logo';
 import { NavBar } from './components/NavBar/NavBar';
 import { ThemeToggler } from './components/ThemeToggler/ThemeToggler';
 import { Container } from 'shared/styles/components/Container.styled';
+import { StyledDiv } from './Header.styled';
 
 export const Header = () => {
   return (
     <header>
       <Container>
-        <Logo />
-        <ThemeToggler />
-        <NavBar />
+        <StyledDiv>
+          <div className="container-left">
+            <Logo />
+            <ThemeToggler />
+          </div>
+          <div className="container-right">
+            <NavBar />
+          </div>
+        </StyledDiv>
       </Container>
     </header>
   );
