145a38b3edb7758171234574352541a8d50f5505
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-05-15 13:35:10 +0300

    random charater page base

--- src/modules/Characters/components/RandomCharacter/RandomCharacter.jsx
+++ src/modules/Characters/components/RandomCharacter/RandomCharacter.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useCharacter } from 'hooks/useCharacter';
+import { useDispatch } from 'react-redux';
 import { RandomButton } from 'shared/components/RandomButton';
+import { getCharacterById } from 'redux/character/thunks';
 
 export const RandomCharacter = () => {
+  const { character, isLoading, error } = useCharacter();
+  const [id, setId] = useState(1);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCharacterById(id));
+  }, [dispatch, id]);
+
+  const handleGetRandomCharacter = () => {
+    setId(2);
+    //generate random id
+  };
+
+  const shouldShowCharacter = character !== null && !isLoading && !error;
+  const shouldShowError = !isLoading && error;
+
   return (
     <>
-      <RandomButton>Random Character</RandomButton>
+      <div>
+        {isLoading && <div>Loading...</div>}
+        {shouldShowCharacter && <div>{character.name}</div>}
+        {shouldShowError && <div>Oops, something went wrong...</div>}
+      </div>
+      <RandomButton onClick={handleGetRandomCharacter}>
+        Random Character
+      </RandomButton>
     </>
   );
 };
