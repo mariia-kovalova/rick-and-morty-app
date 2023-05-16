d6e5b07f9695202bfa86875d07088d173efbf117
AntonVak <antonv.snab@gmail.com>
2023-05-16 11:06:01 +0300

    add episode page

---
+++ src/modules/Episodes/components/EpisodeCardInfo.jsx
@@ -0,0 +1,23 @@
+import { FlexCard } from '../Episodes.styled';
+import { TextInfo } from './EpisodeCard/EpisodeCard.styled';
+
+
+const EpisodeCardInfo = ({ episode }) => {
+  const { name, air_date, episode: number } = episode;
+  console.log(name);
+  return (
+    <FlexCard>
+      <TextInfo>
+        <h3>Episode name: {name}</h3>
+      </TextInfo>
+      <TextInfo>
+        <p>Air date: {air_date}</p>
+      </TextInfo>
+      <TextInfo>
+        <p>Episode: {number}</p>
+      </TextInfo>
+    </FlexCard>
+  );
+};
+
+export default EpisodeCardInfo;
