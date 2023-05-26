c0e2cdcf30cb4607ba379eea45aeedf1d4f73dd4
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-05-26 16:00:36 +0300

    upg

--- src/modules/Characters/components/Character/Character.styled.jsx
+++ src/modules/Characters/components/Character/Character.styled.jsx
@@ -5,13 +5,12 @@ import { CardWrap } from 'shared/styles/components/CardDecoration.styled';
 export const CharacterCardWrap = styled(CardWrap)`
   position: relative;
 
-  &::before {
-    top: -8px;
-  }
-
   @media screen and (min-width: ${tablet}) {
     display: grid;
     grid-template-columns: 235px 1fr;
     gap: 60px;
+    &::before {
+      top: -8px;
+    }
   }
 `;
