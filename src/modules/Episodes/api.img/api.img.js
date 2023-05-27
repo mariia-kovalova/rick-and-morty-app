8c0eeb8f40e82c23efe2eb48fd028ca867a44fdd
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-05-28 01:13:03 +0300

    img upg squoosh

--- src/modules/Episodes/api.img/api.img.js
+++ src/modules/Episodes/api.img/api.img.js
@@ -1,12 +1,12 @@
-import epImg1 from '../../../shared/images/episode1.jpeg';
-import epImg2 from '../../../shared/images/episode2.jpeg';
-import epImg3 from '../../../shared/images/episode3.jpeg';
-import epImg4 from '../../../shared/images/episode4.jpeg';
+import epImg1 from '../../../shared/images/episode1.jpg';
+import epImg2 from '../../../shared/images/episode2.jpg';
+import epImg3 from '../../../shared/images/episode3.jpg';
+import epImg4 from '../../../shared/images/episode4.jpg';
 import epImg5 from '../../../shared/images/episode5.jpg';
-import epImg6 from '../../../shared/images/episode6.jpeg';
-import epImg7 from '../../../shared/images/episode7.jpeg';
-import epImg8 from '../../../shared/images/episode8.jpeg';
-import epImg9 from '../../../shared/images/episode9.jpeg';
+import epImg6 from '../../../shared/images/episode6.jpg';
+import epImg7 from '../../../shared/images/episode7.jpg';
+import epImg8 from '../../../shared/images/episode8.jpg';
+import epImg9 from '../../../shared/images/episode9.jpg';
 import epImg10 from '../../../shared/images/episode10.jpg';
 import epImg11 from '../../../shared/images/episode11.jpg';
 import epImg12 from '../../../shared/images/episode12.jpg';
@@ -16,4 +16,22 @@ import epImg15 from '../../../shared/images/episode15.jpg';
 import epImg16 from '../../../shared/images/episode16.jpg';
 import epImg17 from '../../../shared/images/episode17.jpg';
 
-export const images = [epImg1, epImg2, epImg3, epImg4, epImg5, epImg6, epImg7, epImg8, epImg9, epImg10, epImg11, epImg12, epImg13, epImg14, epImg15, epImg16, epImg17];
+export const images = [
+  epImg1,
+  epImg2,
+  epImg3,
+  epImg4,
+  epImg5,
+  epImg6,
+  epImg7,
+  epImg8,
+  epImg9,
+  epImg10,
+  epImg11,
+  epImg12,
+  epImg13,
+  epImg14,
+  epImg15,
+  epImg16,
+  epImg17,
+];
--- src/shared/styles/components/Section.styled.jsx
+++ src/shared/styles/components/Section.styled.jsx
@@ -1,11 +1,6 @@
 import styled from '@emotion/styled';
-import locationPageBackground from '../../images/locationPageBackground.png';
 
 export const Section = styled.section`
   padding-top: 20px;
   padding-bottom: 20px;
-
-  /* &.location-page {
-    background: url(${locationPageBackground});
-  } */
 `;
